480ee91040817e836ab7ad6c74e3164f0d54e406
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-06 15:17:59 +0300

    Memory copy solution

--- TRIALS/test.c
+++ TRIALS/test.c
@@ -5,8 +5,8 @@ int main(int argc, char const *argv[])
     /* code */
     int i = 0;
     int size = 98;
-    char buffer[98] = {0x00};
-    char b = "0x01";
+    char buffer[98] = {0};
+    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
     for (; i < size; i++)
     {
