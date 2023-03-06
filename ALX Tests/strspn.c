4a2df4d6e98715ff480b828eacce24ce3ff1be07
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-06 18:29:27 +0300

    strspn solution

---
+++ ALX Tests/strspn.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+unsigned int _strspn(char *s, char *accept);
+
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int i = 0, j, sum = 0;
+
+    while (s[i] != '\0')
+    {
+        j = 0;
+        while (accept[j] != '\0')
+        {
+            if (s[i] == accept[j])
+            {
+                sum += 1;
+                break;
+            }  
+            j++;
+        }
+        if (accept[j] == '\0')
+            break;
+        
+        i++;
+    }
+    return (sum);
+}
