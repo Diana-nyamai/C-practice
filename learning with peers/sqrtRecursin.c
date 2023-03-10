b9efc92ad61a824cdec22a71ab69dc54103c85ed
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-10 14:48:08 +0300

    sqrtrecursion update

---
+++ learning with peers/sqrtRecursin.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+
+int sqrthelper(int n, int val)
+{
+    if (val % (n/val) == 0)
+    {
+        if (val * (n / val) == n)
+        {
+            return val;
+        }
+        else
+            return (-1);
+    }
+
+    return sqrthelper(n, val + 1);
+}
+
+int _sqrt_recursion(int n)
+{
+  if (n < 0)
+    return (-1);
+  if (n == 0)
+    return (0);
+  if (n == 1)
+    return (1);
+  
+  return sqrthelper(n, 2);
+}
+int main(void)
+{
+    int r;
+
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
+    printf("%d\n", r);
+    return (0);
+}
---
+++ learning with peers/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i = 1/10;
+    printf("%d ", i);
+    return 0;
+}
