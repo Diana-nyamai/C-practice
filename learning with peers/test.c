38ff80d7f01ceeb0d5fce3ee6ba2a52469ce42e0
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-14 08:46:38 +0300

    test

--- learning with peers/test.c
+++ learning with peers/test.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char const **argv)
 {
-    int i = 1/10;
-    printf("%d ", i);
-    return 0;
+   int n;
+   char *arr;
+
+   n = 5;
+   arr = malloc(sizeof(char) * n);
+
+   arr[0] = 'c';
+   arr[1] = 'o';
+   arr[2] = 'o';
+   arr[3] = 'l';
+
+   printf("%s\n ", arr);
+   
+   return (0);
 }
+
