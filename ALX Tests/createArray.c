c4dfb9d163e7eb2f187b65c74c61e2719c06c3e5
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-17 17:49:29 +0300

    Create array Solution

--- ALX Tests/createArray.c
+++ ALX Tests/createArray.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *create_array(unsigned int size, char c)
+{
+    char *arr;
+    unsigned int i;
+
+    if (size == 0)
+        return NULL;
+
+    arr = malloc(sizeof(char) * size);
+
+    if (arr == NULL)
+        return NULL;
+    for (i = 0; i < size; i++)
+    {
+        arr[i] = c;
+    }
+  arr[i] = '\0';
+  return (arr);
+}
 
 void simple_print_buffer(char *buffer, unsigned int size)
 {
